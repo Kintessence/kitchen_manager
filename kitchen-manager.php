9bb6791de83ee5f14608a0f50a0a8c3c972b3cf5
Fabio Birous <usocomum@gmail.com>
2026-08-18 01:11:17 -0300

    feat: versão atualizada do Kitchen Manager

--- kitchen-manager.php
+++ kitchen-manager.php
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 }
 
 // 1. Constantes Globais
-define('KM_VERSION', '1.0.0');
+define('KM_VERSION', '1.1.0');
 define('KM_PLUGIN_FILE', __FILE__);
 define('KM_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('KM_PLUGIN_URL', plugin_dir_url(__FILE__));
